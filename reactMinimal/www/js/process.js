67dc5e6154d03518248b6896b76f844e4bf09633
Elsa <andrimarc211@gmail.com>
2017-12-05 07:17:54 -0800

    reactanime

---
+++ reactMinimal/www/js/process.js
@@ -0,0 +1,341 @@
+let process = {
+	menu:function(){
+		let content = <ul><center>
+			<a href='#' onClick={process.page1}>Boku no Hero Academia</a><br/>
+			<a href='#' onClick={process.page2}>Death Note</a><br/>
+			<a href='#' onClick={process.page3}>Fullmetal Alchemist: Brotherhood</a><br/>
+			<a href='#' onClick={process.page4}>Gintama</a><br/>
+			<a href='#' onClick={process.page5}>Haikyuu!!</a><br/>
+			<a href='#' onClick={process.page6}>Hajime no Ippo</a><br/>
+			<a href='#' onClick={process.page7}>Hunter x Hunter</a><br/>
+			<a href='#' onClick={process.page8}>Kimi no Na wa.</a><br/>
+			<a href='#' onClick={process.page9}>One-Punch</a><br/>
+			<a href='#' onClick={process.page10}>Shingeki no Kyojin</a><br/>
+
+
+		</center></ul>;
+		ReactDOM.render(content,document.getElementById('root'));
+	},
+	page1:function(){
+		let content = <div>
+		<center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/boku.jpg"></img><br/>
+				<h1><b>Boku no Hero Academia</b></h1>
+				<u>Alternative Titles</u><br/>
+				English: My Hero Academia<br/>
+				<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 13<br/>
+Status: Finished Airing<br/>
+Aired: Apr 3, 2016 to Jun 26, 2016<br/>
+Premiered: Spring 2016<br/>
+Broadcast: Sundays at 17:00 (JST)<br/>
+Producers: Dentsu, Mainichi Broadcasting System, Movic, TOHO animation, Shueisha<br/>
+Licensors: Funimation<br/>
+Studios: Bones<br/>
+Source: Manga<br/>
+Genres: Action, Comedy, School, Shounen, Super Power<br/>
+Duration: 24 min. per ep.<br/>
+Rating: PG-13 - Teens 13 or old<br/>
+
+<a href="https://www.youtube.com/watch?v=wIb3nnOeves">Trailer !!!</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page2:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/death.jpg"></img><br/>
+				<h1>Death Note</h1><br/>
+				<u>Alternative Titles</u><br/>
+English: Death Note<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 37<br/>
+Status: Finished Airing<br/>
+Aired: Oct 4, 2006 to Jun 27, 2007<br/>
+Premiered: Fall 2006<br/>
+Broadcast: Wednesdays at 00:56 (JST)<br/>
+Producers: VAP, Konami, Ashi Production, Nippon Television Network,Shueisha<br/>
+Licensors: Viz Media<br/>
+Studios: Madhouse<br/>
+Source: Manga<br/>
+Genres: Mystery, Police, Psychological, Supernatural, Thriller<br/>
+Duration: 23 min. per ep.<br/>
+Rating: R - 17+ (violence & profanity)<br/>
+
+
+<a href="https://www.youtube.com/watch?v=tJZtOrm-WPk">Trailer !!!</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page3:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/full.jpg"></img><br/>
+				<h1>Fullmetal Alchemist: Brotherhood</h1><br/>
+				<u>Alternative Titles</u><br/>
+English: Fullmetal Alchemist: Brotherhood<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 64<br/>
+Status: Finished Airing<br/>
+Aired: Apr 5, 2009 to Jul 4, 2010<br/>
+Premiered: Spring 2009<br/>
+Broadcast: Sundays at 17:00 (JST)<br/>
+Producers: Aniplex, Square Enix,Mainichi Broadcasting System, Studio Moriken<br/>
+Licensors: Funimation, Aniplex of America<br/>
+Studios: Bones<br/>
+Source: Manga<br/>
+Genres: Action, Military, Adventure, Comedy, Drama, Magic, Fantasy, Shounen<br/>
+Duration: 24 min. per ep.<br/>
+Rating: R - 17+ (violence & profanity)<br/>
+
+
+<a href="https://www.youtube.com/watch?v=BOm_PAI2goo">Trailer !!!</a>
+
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page4:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/gin.jpg"></img><br/>
+				
+
+ 		<h1>Gintama</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Gintama<br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 51<br/>
+Status: Finished Airing<br/>
+Aired: Apr 8, 2015 to Mar 30, 2016<br/>
+Premiered: Spring 2015<br/>
+Broadcast: Wednesdays at 18:00 (JST)<br/>
+Producers: TV Tokyo, Aniplex, Dentsu<br/>
+Licensors: None found, add some<br/>
+Studios: Bandai Namco Pictures<br/>
+Source: Manga<br/>
+Genres: Action, Comedy, Historical, Parody, Samurai, Sci-Fi, Shounen<br/>
+Duration: 24 min. per ep.<br/>
+Rating: R - 17+ (violence & profanity)<br/>
+
+
+<a href="https://www.youtube.com/watch?v=djJ8QsnxsOk">Trailer !!!</a>
+<br/>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+page5:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/gin.jpg"></img><br/>
+
+ 		<h1>Haikyuu!!</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Haikyu!!<br/>
+Synonyms: High Kyuu!!, HQ!!<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 25<br/>
+Status: Finished Airing<br/>
+Aired: Apr 6, 2014 to Sep 21, 2014<br/>
+Premiered: Spring 2014<br/>
+Broadcast: Sundays at 17:00 (JST)<br/>
+Producers: Mainichi Broadcasting System, TOHO animation, Shueisha<br/>
+Licensors: SentaiFilmworks<br/>
+Studios: Production I.G<br/>
+Source: Manga<br/>
+Genres: Comedy, Sports, Drama, School, Shounen<br/>
+Duration: 24 min. per ep.<br/>
+Rating: PG-13 - Teens 13 or older<br/>
+
+
+
+<a href="https://www.youtube.com/watch?v=rxlgoz22-bY">Trailer !!!</a>
+				
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page6:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/hajime.jpg"></img><br/>
+		<h1>Hajime no Ippo</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Fighting Spirit<br/>
+Synonyms: The First Step, Hajime no Ippo: The Fighting<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 75<br/>
+Status: Finished Airing<br/>
+Aired: Oct 4, 2000 to Mar 27, 2002<br/>
+Premiered: Fall 2000<br/>
+Broadcast: Tuesdays at 00:50 (JST)<br/>
+Producers: VAP<br/>
+Licensors: Geneon Entertainment USA<br/>
+Studios: Madhouse<br/>
+Source: Manga<br/>
+Genres: Comedy, Sports, Drama, Shounen<br/>
+Duration: 23 min. per ep.<br/>
+Rating: PG-13 - Teens 13 or older<br/>
+
+<a href="https://www.youtube.com/watch?v=FNpjwxk8-a8">Trailer !!!</a>
+				
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page7:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/hunter.jpg"></img><br/>
+				<h1>Hunter x Hunter</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Hunter X Hunter<br/>
+Synonyms: HxH<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 148<br/>
+Status: Finished Airing<br/>
+Aired: Oct 2, 2011 to Sep 24, 2014<br/>
+Premiered: Fall 2011<br/>
+Broadcast: Sundays at 10:55 (JST)<br/>
+Producers: VAP, Nippon Television Network, Shueisha<br/>
+Licensors: Viz Media<br/>
+Studios: Madhouse<br/>
+Source: Manga<br/>
+Genres: Action, Adventure, Super Power, Shounen<br/>
+Duration: 23 min. per ep.<br/>
+Rating: PG-13 - Teens 13 or older<br/>
+
+
+<a href="https://www.youtube.com/watch?v=d6kBeJjTGnY">Trailer !!!</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	
+	},
+	page8:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/kimi.jpg"></img><br/>
+				<h1>Kimi no Na wa.</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Your Name.<br/><br/>
+
+<u>Information</u><br/>
+Type: Movie<br/>
+Episodes: 1<br/>
+Status: Finished Airing<br/>
+Aired: Aug 26, 2016<br/>
+Producers: KadokawaShoten, Toho,Sound Team Don Juan, Lawson HMV Entertainment, Amuse, East Japan Marketing & Communications<br/>
+Licensors: Funimation<br/>
+Studios: CoMix Wave Films<br/>
+Source: Original<br/>
+Genres: Supernatural, Drama, Romance, School<br/>
+Duration: 1 hr. 46 min.<br/>
+Rating: PG-13 - Teens 13 or older<br/>
+
+
+
+<a href="https://www.youtube.com/watch?v=hRfHcp2GjVI">Trailer !!!</a>
+
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+	page9:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/ONE.jpg"></img><br/>
+		<h1>One Punch Man</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: One Punch Man<br/>
+Synonyms: One Punch-Man, One-Punch Man, OPM<br/><br/>
+
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 12<br/>
+Status: Finished Airing<br/>
+Aired: Oct 5, 2015 to Dec 21, 2015<br/>
+Premiered: Fall 2015<br/>
+Broadcast: Mondays at 01:05 (JST)<br/>
+Producers: TV Tokyo, Bandai Visual,Lantis, Asatsu DK, Banpresto, Good Smile Company, Shueisha, East Japan Marketing & Communications<br/>
+Licensors: Viz Media<br/>
+Studios: Madhouse<br/>
+Source: Web manga<br/>
+Genres: Action, Sci-Fi, Comedy, Parody, Super Power, Supernatural, Seinen<br/>
+Duration: 24 min. per ep.<br/>
+Rating: R - 17+ (violence & profanity)<br/>
+
+
+
+<a href="https://www.youtube.com/watch?v=2JAElThbKrI">Trailer !!!</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+page10:function(){
+		let content = <div><center>
+		<a href='#' onClick={process.menu}>back</a><br/>
+		<img src="img/aot.jpg"></img><br/>
+<h1>Shingeki no Kyojin</h1><br/>
+
+<u>Alternative Titles</u><br/>
+English: Attack on Titan<br/>
+Synonyms: AOT<br/><br/>
+
+<u>Information</u><br/>
+Type: TV<br/>
+Episodes: 25<br/>
+Status: Finished Airing<br/>
+Aired: Apr 7, 2013 to Sep 29, 2013<br/>
+Premiered: Spring 2013<br/>
+Broadcast: Sundays at 01:58 (JST)<br/>
+Producers: Production I.G, Dentsu,Mainichi Broadcasting System, <br/>
+Pony Canyon, Kodansha, Mad Box, Shingeki no Kyojin Team, Pony Canyon Enterprise<br/>
+Licensors: Funimation<br/>
+Studios: Wit Studio<br/>
+Source: Manga<br/>
+Genres: Action, Military, Super Power, Drama, Fantasy, Shounen<br/>
+Duration: 24 min. per ep.<br/>
+Rating: R - 17+ (violence & profanity)<br/>
+
+
+
+<a href="https://www.youtube.com/watch?v=af6O_McpSEA">Trailer !!!</a>
+			</center></div>;
+			ReactDOM.render(content,document.getElementById('root'));
+	},
+
+
+	loading:function(){
+		let content = <div>
+					<center><h1>Otaku Alert...</h1></center>
+				</div>;
+		ReactDOM.render(content,document.getElementById('root'));
+	}
+}
+
+process.loading();
+setTimeout(function(){
+	process.menu();
+},5000);
